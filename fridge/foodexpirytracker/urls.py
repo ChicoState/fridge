97d9e8eea66f4f2fef5625fbb84b6b7dbd3570a4
Amritvir Randhawa <arandhawa@mail.csuchico.edu>
2022-10-21 17:00:52 -0700

    added new logo, fixed url issue

--- fridge/foodexpirytracker/urls.py
+++ fridge/foodexpirytracker/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path as url
 from django.contrib.auth import views as auth_views
 from app1 import views as accounts_views
 from app1.views import list_item, meals, recipes, search, item_update, item_delete, profile
